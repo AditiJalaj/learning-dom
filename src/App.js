7d9ae7ca5cd29ff11786001bb73f63aaaade6f67
ajalaj <ajalaj@AJALAJ-IN.oradev.oraclecorp.com>
2021-05-17 17:46:22 +0530

    lesson 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App=()=>{
+
+
+  //below will give the 2th child of element with id wrapper and element with classNamename span-name
+  const click=()=>{
+    console.log(document.querySelector('#wrapper li:nth-child(1)'))
+  }
+
+  return(
+    <div id="wrapper">
+    <ul>
+    <li className="list">1st</li>
+    <li className="list">2nd item</li>
+    <li  className="list">3rd</li>
+    <span className="span-name">Hello namaste</span>
+    <br></br>
+    <button onClick={click}>CLICK to get the log</button>
+    </ul>
     </div>
-  );
+
+  )
 }
 
 export default App;
