25e0cecab6d1b748dd7c7b963e424949b69c0faf
ajalaj <ajalaj@AJALAJ-IN.oradev.oraclecorp.com>
2021-05-17 18:24:00 +0530

    textContent

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 const App=()=>{
 
 
-  //below will give the 2th child of element with id wrapper and element with classNamename span-name
+  
   const click=()=>{
+    //below will give the 2th child of element with id wrapper and element with classNamename span-name
     //console.log(document.querySelector('#wrapper li:nth-child(1)'))
     const spanList=(document.querySelectorAll('#wrapper  li .span-name'))
     spanList.forEach(function(item){
-      console.log(item)
+     console.log(item.textContent)
+     //the above will give the text on that span
     })
   }
 
@@ -21,8 +23,8 @@ const App=()=>{
     <li className="list">1st</li>
     <li className="list">2nd item</li>
     <li className="list-name">3rd
-    <span className='span-name'> hello from inside list </span>
-    <span className='span-name'> hello from inside list 2</span>
+    <span className='span-name'> hello from inside list  </span>
+    <span className='span-name'> hello from inside list 2    </span>
     </li>
     <span className="span-name">Hello namaste</span>
     <br></br>
