1995d97b81f9dded327e5fdbca5ff03c4cafbbce
ajalaj <ajalaj@AJALAJ-IN.oradev.oraclecorp.com>
2021-05-17 18:06:07 +0530

    looping over elements returned from querySelectorAll

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ const App=()=>{
   //below will give the 2th child of element with id wrapper and element with classNamename span-name
   const click=()=>{
     //console.log(document.querySelector('#wrapper li:nth-child(1)'))
-    console.log(document.querySelector('#wrapper  li .span-name'))
+    const spanList=(document.querySelectorAll('#wrapper  li .span-name'))
+    spanList.forEach(function(item){
+      console.log(item)
+    })
   }
 
 
@@ -19,8 +22,8 @@ const App=()=>{
     <li className="list">2nd item</li>
     <li className="list-name">3rd
     <span className='span-name'> hello from inside list </span>
-    <span > hello from inside list 2</span>
-     </li>
+    <span className='span-name'> hello from inside list 2</span>
+    </li>
     <span className="span-name">Hello namaste</span>
     <br></br>
     <button onClick={click}>CLICK to get the log</button>
